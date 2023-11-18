437eb8b8dba43eec5a3ec8bc1e0260db2ed6601e
SantiRCruz <davidsantiagocruz02@gmail.com>
2023-11-18 16:36:57 -0500

    finish stock app tutorial

---
+++ app/src/main/java/com/example/stockmarket/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.stockmarket
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.stockmarket.presentation.NavGraphs
+import com.example.stockmarket.ui.theme.StockMarketTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StockMarketTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DestinationsNavHost(navGraph = NavGraphs.root)
+                }
+            }
+        }
+    }
+}
+
